163d33a82a52d796885286ce78783a7d27e4de55
Pedro Henrique do Rosário <pdrorosa18912@gmail.com>
2021-09-15 21:50:03 -0300

    Implementando server e o carregamento das informações de operadoras e contatos por request.

--- js/controllers/listaTelefonicaCtrl.js
+++ js/controllers/listaTelefonicaCtrl.js
@@ -1,17 +1,27 @@
 angular.module("listaTelefonica").controller("listaTelefonicaCtrl", function ($scope, $filter, $http) {
     $scope.nameAplication = "Lista Telefônica";
-    $scope.contatos = [
-        { nome: $filter('uppercase')("Pedro"), data: new Date(), telefone: "999997777", cor: "red" },
-        { nome: "João", data: new Date(), telefone: "999997777", cor: "green" },
-        { nome: "Tiago", data: new Date(), telefone: "988886666", cor: "blue" },
-    ];
 
-    $scope.operadoras = [
-        { nome: "Oi", codigo: 14, categoria: "Celular", preco: 2 },
-        { nome: "Vivo", codigo: 15, preco: 1, categoria: "Celular" },
-        { nome: "Tim", codigo: 16, preco: 1.50, categoria: "Celular" },
-        { nome: "Claro/Net", codigo: 47, preco: 1.50, categoria: "Fixo" },
-    ];
+    let carregarContatos = () => $http.get("http://localhost:8888/contatos")
+        .success(function (data) {
+            $scope.contatos = data;
+        });
+
+    let carregarOperadoras = () => $http.get("http://localhost:8888/operadoras")
+        .success(function (data) {
+            console.log(data);
+            $scope.operadoras = data;
+        });
+
+    let carregaInformacao = () => {
+        carregarContatos();
+        carregarOperadoras();
+    }
+
+    carregaInformacao();
+
+    $scope.contatos = [];
+
+    $scope.operadoras = [];
 
     $scope.adicionarContato = function (contato) {
         $scope.contatos.push(angular.copy(contato));
